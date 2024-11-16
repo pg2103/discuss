349e06625e3cef5409f18ea7b4a6f9b36531513b
Prakhar Gupta <prakhar.gupta.212003@gmail.com>
2024-11-16 18:06:18 +0530

    enable sign out and caching issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,25 +6,47 @@ import {
     NavbarItem,
     Input,
     Avatar,
-    Button
+    Button,
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+    
 
 } from "@nextui-org/react";
+import * as actions from '@/actions';
 import Link from "next/link";
 export default async function Header() {
     const session =await auth();
     
     let authContent:React.ReactNode;
     if(session?.user){
-        authContent=<Avatar src={session.user.image||''}/>
+        authContent=
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Avatar src={session.user.image||''}/>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="p-4">
+                    <form action={actions.signOut}>
+                        <Button type="submit">Sign Out</Button>
+                    </form>
+                </div>
+            </PopoverContent>
+        </Popover>
+            
     }else{
         authContent=<>
             <NavbarItem>
-                <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                <form action={actions.signIn}>
+                    <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                </form>
             </NavbarItem>
             <NavbarItem>
-                <Button type="submit" color="primary" variant="flat">Sign Up</Button>
-            </NavbarItem>
-        </>
+                <form action={actions.signIn}>
+                    <Button type="submit" color="primary" variant="flat">Sign Up</Button>
+                </form>
+          </NavbarItem>
+        </> 
     }
 
     return(
